e746fece03a821fd4d84b7b5e06ef78c33e3adf6
gaster516 <71486036+gaster516@users.noreply.github.com>
2023-03-11 11:35:22 +0800

    完成面向 動畫

---
+++ Assets/腳本/control.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ctl
+{
+    public class control : MonoBehaviour
+    {
+        [SerializeField, Header("目標物件")]
+        private Transform target;
+        private void Update()
+        {
+            LookAtTarget();
+        }
+        /// <summary>
+        /// 面向
+        /// </summary>
+        private void LookAtTarget()
+        {
+            //面向
+            //target
+            transform.LookAt(target);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);        }
+
+    }
+}
